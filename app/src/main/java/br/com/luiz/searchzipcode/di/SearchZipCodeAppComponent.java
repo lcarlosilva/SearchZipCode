eb7efda9810d14095520f3c93cb38ed6fe6e5a67
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2024-04-08 16:09:18 -0300

    feat: create domain module

--- app/src/main/java/br/com/luiz/searchzipcode/di/SearchZipCodeAppComponent.java
+++ app/src/main/java/br/com/luiz/searchzipcode/di/SearchZipCodeAppComponent.java
@@ -3,11 +3,16 @@ package br.com.luiz.searchzipcode.di;
 import javax.inject.Singleton;
 
 import br.com.luiz.data.di.DataModule;
+import br.com.luiz.domain.di.DomainModule;
 import br.com.luiz.searchzipcode.MainActivity;
 import dagger.Component;
 
 @Singleton
-@Component(modules = {SearchZipCodeAppModule.class, DataModule.class})
+@Component(modules = {
+        SearchZipCodeAppModule.class,
+        DataModule.class,
+        DomainModule.class
+})
 public interface SearchZipCodeAppComponent {
     void inject(MainActivity mainActivity);
 }
---
+++ domain/src/main/java/br/com/luiz/domain/di/DomainModule.java
@@ -0,0 +1,9 @@
+package br.com.luiz.domain.di;
+
+
+import dagger.Module;
+
+@Module
+public class DomainModule {
+
+}
